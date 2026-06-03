101091bf26be97658400fe70f751a615f8a446c1
Andrii Kalashnikov <kotikfop@gmail.com>
2026-06-03 15:54:38 +0300

    config fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/test-webshark-attempt2/',
+  base: '/test-web-shark2/',
   plugins: [react()],
 });
